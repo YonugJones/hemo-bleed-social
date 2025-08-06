b02dbe7910bf5d3bd75e2704499ff362c496ab64
Peter Kerfoot <peterckerfoot@gmail.com>
2025-08-06 08:34:37 -0500

    added auth routes

---
+++ frontend/src/router/index.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from 'react-router-dom'
+import AuthLayout from '../layouts/AuthLayout'
+import LandingPage from '../pages/LandingPage'
+import Login from '../auth/Login'
+import Signup from '../auth/Signup'
+
+export const router = createBrowserRouter([
+  {
+    element: <AuthLayout />,
+    children: [
+      { path: '/', element: <LandingPage /> },
+      { path: '/login', element: <Login /> },
+      { path: '/signup', element: <Signup /> },
+    ],
+  },
+])
