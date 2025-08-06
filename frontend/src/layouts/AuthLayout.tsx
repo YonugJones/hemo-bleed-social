d0ed365899e5c65bf843521334ab49b83eabedc3
Peter Kerfoot <peterckerfoot@gmail.com>
2025-08-06 08:31:43 -0500

    removed paddiing

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 const AuthLayout = () => {
   return (
-    <main className='min-h-screen flex flex-col items-center justify-center p-4'>
+    <main className='min-h-screen flex flex-col items-center justify-center'>
       <Outlet />
     </main>
   )
