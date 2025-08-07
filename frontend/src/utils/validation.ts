a67d9821d578f3dd5f5b5e5152b66f5daf852978
Peter Kerfoot <peterckerfoot@gmail.com>
2025-08-07 14:42:41 -0500

    removed special chars variable

--- frontend/src/utils/validation.ts
+++ frontend/src/utils/validation.ts
@@ -2,4 +2,3 @@ export const USERNAME_REGEX = /^\S{3,24}$/
 export const EMAIL_REGEX = /^[\w.-]+@([\w-]+\.)+[\w-]{2,4}$/
 export const PASSWORD_REGEX =
   /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{10,}$/
-export const specialChars = '!?+-_,.=@#$%^&*|<>'
