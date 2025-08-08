3ec7e3fbfee4dec8edf157e99c409870032ac201
Peter Kerfoot <peterckerfoot@gmail.com>
2025-08-08 09:52:04 -0500

    styled mobile layout

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -2,27 +2,18 @@ import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
   return (
-    <>
-      <div className='flex items-center text-[var(--green)] gap-5 mb-5'>
-        <img src='https://placehold.co/60' alt='Hemo Bleed Social Logo' />
-        <div className='flex flex-col justify-center items-center'>
-          <h1 className='jaro-logo text-6xl'>Hemo Bleed Social</h1>
-          <h2>Live. Log. Learn.</h2>
-        </div>
-      </div>
-      <div className='flex gap-5'>
-        <Link to='/signup'>
-          <button className='bg-[var(--green)] hover:bg-[var(--light-green)] text-white font-semibold py-2 px-4 rounded shadow cursor-pointer'>
-            Create your account
-          </button>
-        </Link>
-        <Link to='/login'>
-          <button className='bg-[var(--green)] hover:bg-[var(--light-green)] text-white font-semibold py-2 px-4 rounded shadow cursor-pointer'>
-            Log in
-          </button>
-        </Link>
-      </div>
-    </>
+    <div className='flex flex-col sm:flex-col md:flex-row justify-center items-center gap-4'>
+      <Link to='/signup'>
+        <button className='w-[150px] sm:w-[175px] m:w-[200px] px-4 py-1 rounded-full text-center bg-[var(--green)] hover:bg-[var(--light-green)] text-white text-xs font-semibold shadow cursor-pointer'>
+          Create your account
+        </button>
+      </Link>
+      <Link to='/login'>
+        <button className='w-[150px] sm:w-[175px] m:w-[200px] px-4 py-1 rounded-full text-center bg-[var(--green)] hover:bg-[var(--light-green)] text-white text-xs font-semibold shadow cursor-pointer'>
+          Log in
+        </button>
+      </Link>
+    </div>
   )
 }
 
