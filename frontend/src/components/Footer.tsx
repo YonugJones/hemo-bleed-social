02911da808d61d5116a06016204da52d6a3c68a0
Peter Kerfoot <peterckerfoot@gmail.com>
2025-08-09 10:15:53 -0500

    added github logo

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
 const Footer = () => {
   return (
     <footer className='text-center py-4 text-sm'>
       &copy; 2025 Peter Kerfoot
+      <a className='ml-2' href='https://github.com/YonugJones?tab=repositories'>
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
     </footer>
   )
 }
