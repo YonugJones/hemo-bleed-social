0e81eb02131f216aa3c2a71cc72dd9afdcb78e9d
Peter Kerfoot <peterckerfoot@gmail.com>
2025-08-10 10:42:27 -0500

    added basic profile skeleton

---
+++ frontend/src/pages/Profile.tsx
@@ -0,0 +1,14 @@
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+
+const Profile = () => {
+  return (
+    <section className='flex flex-col'>
+      <header className=''>
+        <h1 className=''>PROFILE</h1>
+      </header>
+    </section>
+  )
+}
+
+export default Profile
