9a3a558c2c9ef4ea9cefdff1ccbfeed719ff5e08
Peter Kerfoot <peterckerfoot@gmail.com>
2025-08-17 13:58:31 -0500

    added validateSignup to auth rote

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const { signup } = require('../controllers/auth')
+const { validateSignup } = require('../middleware/validateInput')
 const router = express.Router()
 /*
 /auth
 */
-router.post('/signup', signup)
+router.post('/signup', validateSignup, signup)
 
 module.exports = router
