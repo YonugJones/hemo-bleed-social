0b6c0f8c2199aa3266a18a06794aae929c181bc0
Peter Kerfoot <peterckerfoot@gmail.com>
2025-08-20 17:33:57 -0500

    adjusted to work with TypeScript

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,23 @@
+import { createContext, useState, type ReactNode } from 'react'
+import type { AuthState } from '../types/auth'
+
+interface AuthContextType {
+  auth: AuthState
+  setAuth: React.Dispatch<React.SetStateAction<AuthState>>
+}
+
+interface AuthProviderProps {
+  children: ReactNode
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined)
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [auth, setAuth] = useState<AuthState>({})
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
