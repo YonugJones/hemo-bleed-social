730b751a084d73c5ba543646ca8317c90970ab59
Peter Kerfoot <peterckerfoot@gmail.com>
2025-08-19 19:53:44 -0500

    added loginUser API function

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,16 @@
+import { axiosPublic } from './axiosConfig'
+import type { SignupFormValues } from '../types/auth'
+
+export const signupUser = async (data: SignupFormValues) => {
+  const res = await axiosPublic.post('/auth/signup', data)
+  return res.data
+}
+
+export const loginUser = async (username: string, password: string) => {
+  const res = await axiosPublic.post(
+    '/auth/login',
+    { username, password },
+    { withCredentials: true }
+  )
+  return res.data
+}
