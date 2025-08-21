3dcae7caa32a53a23c645f20c3bb33fab1b57eff
Peter Kerfoot <peterckerfoot@gmail.com>
2025-08-21 15:54:41 -0500

    updated style for buttonClass

---
+++ frontend/src/styles/classNames.ts
@@ -0,0 +1,6 @@
+export const formClass =
+  'flex flex-col items-center gap-y-4 gap-x-2 w-full max-w-md mx-auto'
+export const inputClass =
+  'w-[150px] sm:w-[175px] px-4 py-1 border border-gray-200 inset-shadow-2xs rounded-full text-center text-xs mx-1'
+export const buttonClass =
+  'text-white text-center cursor-pointer w-[175px] px-4 py-2 rounded-full bg-[var(--green)] hover:bg-[var(--light-green)] text-white font-semibold shadow cursor-pointer'
