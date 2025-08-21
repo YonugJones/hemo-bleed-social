778c85c6ac4aba693a4805993f80ead376b7b053
Peter Kerfoot <peterckerfoot@gmail.com>
2025-08-21 15:51:11 -0500

    created instances of axios for API calls

--- frontend/src/api/axiosConfig.ts
+++ frontend/src/api/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'http://localhost:3333'
 
 // creates axiosInstance without credentials for use by authApi calls
 export const axiosPublic = axios.create({
