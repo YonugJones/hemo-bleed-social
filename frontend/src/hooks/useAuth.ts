923264ccb8aa109892bf22fc57b99ee13e3fb929
Peter Kerfoot <peterckerfoot@gmail.com>
2025-08-21 15:48:55 -0500

    created useAuth hook for protected route

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -4,4 +4,5 @@ import AuthContext from '../context/AuthContext'
 export const useAuth = () => {
   const context = useContext(AuthContext)
   if (!context) throw new Error('useAuth must be used with AuthProvider')
+  return context
 }
