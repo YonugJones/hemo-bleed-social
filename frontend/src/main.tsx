e169a4f186a379f68d1fea124db3805478c53f2f
Peter Kerfoot <peterckerfoot@gmail.com>
2025-08-21 15:57:17 -0500

    wrapped app with AuthContext

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
+import { AuthProvider } from './context/AuthContext'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
   </StrictMode>
 )
