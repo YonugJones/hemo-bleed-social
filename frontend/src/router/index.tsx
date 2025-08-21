88d9fbe2e5aea710017e68e04618c5f259ee836b
Peter Kerfoot <peterckerfoot@gmail.com>
2025-08-21 15:52:33 -0500

    wrapped routes with protected route

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,19 +1,44 @@
 import { createBrowserRouter } from 'react-router-dom'
 import AuthLayout from '../layouts/AuthLayout'
-import LandingPage from '../pages/LandingPage'
-import Login from '../auth/Login'
-import Signup from '../auth/Signup'
+import AuthHome from '../components/AuthHome'
+import Login from '../components/Login'
+import Signup from '../components/Signup'
 import LearnMore from '../pages/LearnMore'
 import NotFound from '../components/NotFound'
+import UserLayout from '../layouts/UserLayout'
+import Protected from './Protected'
+import Profile from '../pages/Profile'
+import Feed from '../pages/Feed'
+import EventForm from '../components/forms/EventForm'
+import CalendarPage from '../pages/CalendarPage'
 
 export const router = createBrowserRouter([
   {
     element: <AuthLayout />,
     children: [
-      { path: '/', element: <LandingPage /> },
+      { path: '/', element: <AuthHome /> },
       { path: '/login', element: <Login /> },
       { path: '/signup', element: <Signup /> },
       { path: '/learn-more', element: <LearnMore /> },
+    ],
+  },
+  {
+    element: <Protected />,
+    children: [
+      {
+        element: <UserLayout />,
+        children: [
+          { path: '/feed', element: <Feed /> },
+          { path: '/calendar', element: <CalendarPage /> },
+          { path: '/messages', element: <div>MESSAGES</div> },
+          { path: '/notifications', element: <div>NOTIFICATIONS</div> },
+          { path: '/profile', element: <Profile /> },
+          {
+            path: '/event',
+            element: <EventForm onSubmit={(data) => console.log(data)} />,
+          },
+        ],
+      },
     ],
   },
   {
