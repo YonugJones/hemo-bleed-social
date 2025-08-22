f8858f329a8339ebcfc08318007b2830cf506981
Peter Kerfoot <peterckerfoot@gmail.com>
2025-08-21 19:03:27 -0500

    added default values to Infusion model

---
+++ backend/prisma/migrations/20250822000231_add_infusion_defaults/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "public"."Infusion" ALTER COLUMN "dosage" SET DEFAULT 0,
+ALTER COLUMN "location" SET DEFAULT '',
+ALTER COLUMN "lotNumbers" SET DEFAULT '',
+ALTER COLUMN "reason" SET DEFAULT 'Prophylaxis';
