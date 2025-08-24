7caeba85813786f861b2b624460be7b395fdd1ff
Peter Kerfoot <peterckerfoot@gmail.com>
2025-08-23 19:01:45 -0500

    added route for updateBleed

--- backend/routes/bleed.js
+++ backend/routes/bleed.js
@@ -4,5 +4,6 @@ const authenticateToken = require('../middleware/auth')
 const router = express.Router()
 
 router.post('/', authenticateToken, bleedController.createBleed)
+router.put('/:bleedId', authenticateToken, bleedController.updateBleed)
 
 module.exports = router
